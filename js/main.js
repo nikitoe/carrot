f3502f34526b8bfcb3412955d618d2f3c4b6e77d
Jiyong Sim <sjy19910222@gmail.com>
2022-02-12 19:35:41 +0900

    JS: Add stop function

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
+const popUp = document.querySelector('.pop-up');
+const popUpText = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -32,13 +36,19 @@ function startGame () {
 };
 
 function stopGame () {
-
+    stopGameTimer();
+    hideGameButton();
+    showPopUpWithText('REPLAY🤞?');
 };
 
 function showStopButton () {
     const icon = gameBtn.querySelector('.fa-play');
     icon.classList.add('fa-stop');
     icon.classList.remove('fa-play');
+};
+
+function hideGameButton () {
+    gameBtn.style.visibility='hidden';
 };
 
 function showTimerAndScore () {
@@ -59,10 +69,19 @@ function startGameTimer () {
     },1000);
 };
 
+function stopGameTimer () {
+    clearInterval(timer);
+};
+
 function updateTimerText (time) {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
     gameTimer.innerText = `${minutes}:${seconds}`;
+};
+
+function showPopUpWithText (text) {
+    popUpText.innerText = text;
+    popUp.classList.remove('pop-up--hide');
 };
 
 function initGame () {
